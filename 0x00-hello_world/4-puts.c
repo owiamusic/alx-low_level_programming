ec75b63cd125937c475b74cd7f3fb7596284ad67
owiamusic <owiamusic@gmail.com>
2023-06-15 10:34:09 -0700

    Print Text

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
+/**
+ * main - a program that prints using puts
+ *
+ * return value - 0 (success)
+*/
+
 int main()
 {
     puts(Programming is like building a multilingual puzzle);
