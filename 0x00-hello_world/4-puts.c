1ec3483d9c283b19ce919108cfe197663bed5e66
owiamusic <owiamusic@gmail.com>
2023-06-15 11:41:50 -0700

    Print Word

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - The program prints the text with a new line
+ * main(void) - The program prints the text with a new line
 */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-
 /**
   * return - The program ends with a return value of (0)                                                                                            
 */
