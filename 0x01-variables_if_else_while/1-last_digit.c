bf85d53acc6b9edd357dcd7e9977073bdbf744e5
owiamusic <owiamusic@gmail.com>
2023-06-16 15:01:11 -0700

    Greater than

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,7 +4,6 @@
 
 /**
  * main- Prints the last digit of a number
- * if its greater than, less than
  * Return: 0 is number is 0
  */
 
@@ -16,11 +15,11 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	lastdigit = n % 10;
-	if (n > 5)
-		printf("Last digit of %d is %d and is greater than 5\n", n, lastdigit);
-	else if (n < 6)
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastdigit);
-	else(n == 0)
-		    printf("Last digit of %d is %d and is 0\n", n, lastdigit);
+	if (lastdigit > 5)
+	printf("Last digit of %d is %d and is greater than 5\n", n, lastdigit);
+	else if (lastdigit == 0)
+	printf("Last digit of %d is %d and is 0\n", n, lastdigit);
+	else
+	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastdigit);
 	return (0);
 }
