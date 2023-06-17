4e0b4d9975161e86ffac07ac4d8319065cfa494a
owiamusic <owiamusic@gmail.com>
2023-06-17 04:40:26 -0700

    Print Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Starting point of program
+ * Return: 0 ends program
+ */
+
+int main(void)
+{
+	int singleDigit = 0;
+
+	for (; singleDigit < 10; singleDigit++)
+	{
+	printf("%d", singleDigit);
+	}
+
+	putchar('\n');
+			
+	return (0);
+
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -5,3 +5,7 @@
 2-print-alphabets - Print alphabet in lowercase
 
 3-print-alphabets - Print alphabet in lowercase and UPPERCASE
+
+4-print-alphabts - Print alphabet in lowercase with some characters
+
+5-print_numbers - Print numbers in base 10 starting with 0
