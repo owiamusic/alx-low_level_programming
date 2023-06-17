885b0bfcfeb9a3244abfcd3ff88d9ba24a0b4b52
owiamusic <owiamusic@gmail.com>
2023-06-17 05:00:50 -0700

    Print Numbers with Char

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Starting point of program
+ * Return: 0 at the end of program
+ */
+
+int main(void)
+{
+	int base10 = 0;
+
+	for (; base10 < 10; base10++)
+	{
+		putchar(base10);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
