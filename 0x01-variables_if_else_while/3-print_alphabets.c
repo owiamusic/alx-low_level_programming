d38af7e7ae369b37cd5b7ab360c45a344eb7fc7e
owiamusic <owiamusic@gmail.com>
2023-06-16 18:39:02 -0700

    Lower and Uppercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ *main - Starting point
+ *Return: - 0 at end point
+ */
+
+int main(void)
+{
+	char lowercase = 'a';
+	char uppercase = 'A';
+
+	while (lowercase <= 'z')
+	{
+		putchar(lowercase);
+		lowercase++;
+	}
+	while (uppercase <= 'Z'; uppercase++)
+	{
+		putchar(uppercase);
+		uppercase++;
+	}
+
+	return (0);
+
+	putchar ('\n');
+
+}
