0a0b47ad3479a14fe06646cc4fb6929c838a5ec0
owiamusic <owiamusic@gmail.com>
2023-06-18 13:19:52 -0700

    Print Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - starting point of program
+ * Return: 0 end point of program
+ */
+
+int main(void)
+{
+	int base16;
+	
+	for (base16 = 0; base16 <= 15; base16++)
+	{
+		if (base16 < 10)
+		{
+			putchar('0'+ base16);
+		}
+		else
+		{
+			putchar('a' + base16 - 10);
+		}
+	}
+	putchar('\n');
+	
+	return (0);
+}
