58c4bba63ec05dce28fea8c22fa2b9e2d1324086
owiamusic <owiamusic@gmail.com>
2023-06-20 11:28:53 -0700

    lowercase 10x

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,15 +6,14 @@
 
 void print_alphabet_x10(void)
 {
-	int alpha10 = 'a'
+	int alpha10;
 	int ten = 0;
 	
 	while (ten < 10)
 	{
-	for (; alpha10 <= 'z';)
+	for (; alpha10 <= 'z'; alpha++)
 	{
 		_putchar(alpha10);
-		alpha10++;
 	}
 
 	_putchar('\n');
