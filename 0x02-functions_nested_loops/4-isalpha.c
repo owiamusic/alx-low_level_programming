1e4b2c2d62a4e8443aee80702b3e6f9d19a181df
owiamusic <owiamusic@gmail.com>
2023-06-20 14:27:03 -0700

    letter to be checked

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,5 +1,11 @@
 #include "main.h"
 
+/**
+ * _isalpha - checks for alphabetic characters
+ * @c: the letter to be checked
+ * Return: 1 is true or 0 if false
+ */
+
 int _isalpha(int c)
 {
 	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
