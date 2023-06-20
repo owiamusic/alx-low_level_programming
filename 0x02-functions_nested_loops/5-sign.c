4f13fbe9b87db8dc09dd00011b2e5d0f8c88405c
owiamusic <owiamusic@gmail.com>
2023-06-20 15:42:58 -0700

    print number sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_sign - print sign of the number
+ * @n: number to be checked
+ * Return: -1 if false, 0 if true, 1 if false
+ */
+
+int print_sign(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+		else
+		{
+			_putchar('0');
+			return (0);
+		}
+	}
+}
