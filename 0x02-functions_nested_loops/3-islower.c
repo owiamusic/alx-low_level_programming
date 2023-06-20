9f4b8d93f48f025e0e9f62711dd7bb2ab09c8f25
owiamusic <owiamusic@gmail.com>
2023-06-20 13:46:59 -0700

    print lower case

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lowercase alphabets
+ */
+
+int _islower(int c)
+{
+	if (c => 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+
+}
