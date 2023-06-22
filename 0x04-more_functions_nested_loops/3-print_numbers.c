4e0bdefdaa30258e30890eba2b42410ba3c6930f
owiamusic <owiamusic@gmail.com>
2023-06-22 09:27:24 -0700

    print numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -4,13 +4,14 @@
  * print_numbers - prints numbers 0-9
  */
 
-void print_numbers(void)
+void print_numbers(int c)
 {
-	int c;
-	
+
 	for (c = 48; c <= 57; c++)
 	{
 		_putchar(c);
-		_putchar('\n');
+
 	}
+
+	_putchar('\n');
 }
