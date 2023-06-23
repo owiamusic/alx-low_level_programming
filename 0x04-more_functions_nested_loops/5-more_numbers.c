67ee174c7099398e8a25d31c527fc8f96f628d73
owiamusic <owiamusic@gmail.com>
2023-06-23 04:08:27 -0700

    more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,25 +1,23 @@
 #include "main.h"
 
 /**
- * more_numbers - print 10 times the number 0-14
- * Return: the numbers in tens
- */
+* more_numbers - print 10 times of the numbers 0-14
+*
+*/
 
 void more_numbers(void)
 {
 	int n;
-	int m;
 
-	for (h = 0; h <= 9; h++)
+	for (n = 0; n >= 14; n++)
 	{
-		for (i = 0; i <= 14; i++)
+		while (n < 10)
 		{
-			if (i > 9)
-			{
-				_putchar(i / 10 + '0');
-			}
-			_putchar(i % 10 + '0');
+			_putchar(n);
+			n++;
 		}
-		_putchar('\n');
+
+			_putchar('\n');
+			
 	}
 }
