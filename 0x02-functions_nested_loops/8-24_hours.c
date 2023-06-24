9028c8d50b2126854232514b06237978ddcdcf01
owiamusic <owiamusic@gmail.com>
2023-06-24 15:30:14 -0700

    Jack Bauer minutes

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * jack_bauer - print minutes of Jack Bauer
+ */
+
+void jack_bauer(void)
+{
+	int t;
+	int s;
+	
+	for (t = 0; t <= 23; t++)
+	{
+		for (s = 0; s <= 59; s++)
+		{
+			_putchar(t / 10 + '0');
+			_putchar(t % 10 + '0');
+			_putchar(':');
+			_putchar(s / 10 + '0');
+			_putchar(s % 10 + '0');
+			_putchar('\n');
+		}
+	}
+}
