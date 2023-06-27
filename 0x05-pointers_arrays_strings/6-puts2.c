3965f114674bb8310f6a40cf91ce335d6b8bad3f
owiamusic <owiamusic@gmail.com>
2023-06-27 16:37:11 -0700

    print characters

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include "2-strlen.c"
 
 /**
  * puts2 - print character of a string, first characters
@@ -7,17 +8,11 @@
 
 void puts2(char *str)
 {
-	int l;
-	int j;
-		
-	while (l != '\0')
-	{
-		l++;
-	}
+	int i;
 
-	for (j = 0; j < l(str); j++)
+	for (i = 0; i < strlen(str); i++)
 	{
-		_putchar(str[j]);
+		_putchar(str[i]);
 	}
 	_putchar('\n');
 }
