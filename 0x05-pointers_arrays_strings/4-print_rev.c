8ed2a36b21413d390720d8f7a7f8125a4c0832c3
owiamusic <owiamusic@gmail.com>
2023-06-26 19:59:14 -0700

    print string reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -4,19 +4,18 @@
  * print_rev - prints string in reverse
  * @s: pointer to the string
  */
+
 void print_rev(char *s)
 {
-    int r = 0;
+	int r = 0;
 
-    while (s[r] != '\0')
-    {
-        r++;
-    }
-
-    for (r = r - 1; r >= 0; r--)
-    {
-        _putchar(s[r]);
-    }
-
-    _putchar('\n');
+	while (s[r] != '\0')
+	{
+		r++;
+	}
+	for (r = r - 1; r >= 0; r--)
+	{
+		_putchar(s[r]);
+	}
+	_putchar('\n');
 }
