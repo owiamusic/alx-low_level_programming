771d01fa1643657bece1fd357a91832d9e111d9e
owiamusic <owiamusic@gmail.com>
2023-07-04 08:16:58 -0700

    locate character in a string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -16,11 +16,10 @@ char *_strchr(char *s, char c)
 	{
 		if (s[i] == c)
 		{
-		return (s + i);
+			return (s + i);
 		}
 	}
 	if (s[i] == c)
-	{
-		return (s + i);
+	return (s + i);
 	return (0);
 }
