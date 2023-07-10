7d3441b29d4182ead41f5987d20aaac27c8c284c
owiamusic <owiamusic@gmail.com>
2023-07-10 13:48:32 -0700

    print numbers

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+#define UNUSED(x) (void)(x)
 
 /**
  * main - Program that prints a number
@@ -8,8 +9,9 @@
  * Return: 0 for success
  */
 
-int main(int argc, char *argv[] __attribute__((unused)))
+int main(int argc, char *argv[])
 {
+	UNUSED(argv);
 	printf("%d \n", argc - 1);
 	return (0);
 }
