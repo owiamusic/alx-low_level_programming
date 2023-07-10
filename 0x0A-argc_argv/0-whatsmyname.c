21f648295d9b75d06f245779e89030863a91ee1e
owiamusic <owiamusic@gmail.com>
2023-07-10 04:50:02 -0700

    program that prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Program that prints its name
+ * @argc: Argument count
+ * @argv: Argument string
+ * Return: 0 indicating success
+ */
+
+int main(int argc, char *argv[]);
+{
+	if (argc > 0)
+	{
+		printf("%s\n", argv[0]);
+	}
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,3 @@
+0-whatsmyname - A program that prints its name
+
+1-args - A program that prints numbers 
