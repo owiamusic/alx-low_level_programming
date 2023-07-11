9faf3fbd48f63641058ddf5408c32a45895c7d47
owiamusic <owiamusic@gmail.com>
2023-07-10 17:13:02 -0700

    multiply two numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -14,7 +14,7 @@ int main(int argc, char *argv[])
 	int arg0 = atoi(argv[1]);
 	int arg1 = atoi(argv[2]);
 
-	if (argc == 3)
+	if (argc != 3 || argv[2] == NULL)
 	{
 		printf("%d\n", (arg0 * arg1));
 		return (0);
