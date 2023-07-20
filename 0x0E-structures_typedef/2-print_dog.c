b85410e8100d9eb8915e27feaf54d7d9368622cc
owiamusic <owiamusic@gmail.com>
2023-07-20 08:52:44 -0700

    struct functions update

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,19 +13,19 @@ void print_dog(struct dog *d)
 		if (d->name == NULL)
 		{
 			printf("Name: (nil)\n");
-		}
 		else
 			printf("Name: %s\n", d->name);
+		}
 		if (d->age == -1.0)
 		{
 			printf("Age: (nil)\n");
-		}
 		else
 			printf("Age: %f\n", d->age);
+		}
 		if (d->owner == NULL)
 		{
 			printf("Owner (nil)\n");
-		}
+		else
 			printf("Owner: %s\n", d->owner);
 	}
 	return (0);
