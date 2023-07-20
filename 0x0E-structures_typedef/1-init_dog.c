e2c145efab3a25e695f645d05efbc415ce180933
owiamusic <owiamusic@gmail.com>
2023-07-20 05:22:07 -0700

    struct function

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include "dog.h"
-#include <stdlib.h>
 
 /**
  * init_dog - Function to initialize a variable type
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,6 +15,6 @@ struct dog
 	char *owner;
 };
 
-#endif
-
 void init_dog(struct dog *d, char *name, float age, char *owner);
+
+#endif
