98147311904e3642a3ef577b9176d5f834f21e31
owiamusic <owiamusic@gmail.com>
2023-07-21 17:40:50 -0700

    execute function

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -13,6 +13,10 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
 
+	if (array == NULL && action == NULL)
+	{
+		exit (1);
+	}
 	for (i = 0; i < size; i++)
 	{
 		action(array[i]);
