fdfdc3269d112888f25b1ac34f0b6c694d6658b2
owiamusic <owiamusic@gmail.com>
2023-07-23 15:15:01 -0700

    print numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -16,20 +16,16 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list print_n;
 	va_start(print_n, n);
 
-	if (separator == NULL)
-	{
-		separator = "";
-	}
 	for (i = 0; i < n; i++)
 	{
 		arg_num = va_arg(print_n, int);
-		printf("%d", arg_num);
 
-		if (i < n - 1)
+		if (i > 0 && separator != NULL)
 		{
 			printf("%s", separator);
+		printf("%d", arg_num);
 		}
 	}
+		va_end(print_n);
 		printf("\n");
-	va_end(print_n);
 }
