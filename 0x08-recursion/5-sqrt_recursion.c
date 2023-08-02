bc166d02bd71d9f7424b56079e09b494dd44546f
owiamusic <owiamusic@gmail.com>
2023-08-02 13:01:28 -0700

    square root recursion

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * help - Help in deciding square root
+ * @i: integer to use
+ * @n: integer for square root
+ * Return: square root value
+ */
+
+int help(int i, int n)
+{
+	int j;
+
+	if (i * i != n)
+	{
+		if (i > n)
+		{
+			return (-1);
+		}
+		j = help(i + 1, n);
+		return (j +1);
+	}
+	return (0);
+}
+
+
+/**
+ * _sqrt_recursion - Returns a natural square root of a number
+ * @n: number to be used
+ * Return: value of square root
+ */
+
+int _sqrt_recursion(int n)
+{
+	int i = 0;
+
+	if (help(i, n) == n && n != 1)
+	{
+		return (-1);
+	}
+	return (help(i, n));
+}
