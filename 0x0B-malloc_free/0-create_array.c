557ca171b8ce95d49873bcd21f258118a53726b5
owiamusic <owiamusic@gmail.com>
2023-08-08 14:58:03 -0700

    create array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+  * create_array - creates an array of chars and initialize
+  * @size: size of array
+  * @c: character to initialize
+  * Return: Null or array pointer
+  */
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+
+	char *create_array = malloc(sizeof(char) * size);
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		create_array[i] = c;
+	}
+		return (create_array);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,5 @@
+# Memory Allocation
+
+* 0-create-array - Creates an array of char and initializes with a specific char
+
+*
