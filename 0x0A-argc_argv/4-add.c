9076bcf5a6fae018667216ce51895bd0e33ee849
owiamusic <owiamusic@gmail.com>
2023-08-08 16:01:30 -0700

    add positive numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,58 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * isString - checks for a string
+ * @s: string to check
+ * Return: boolean
+ */
+int isString(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (!isdigit(s[i]))
+		{
+			return (0);
+		}
+	}
+	return (1);
+}
+
+/**
+ * main - main function
+ * @argc: argumentc
+ * @argv: vector of arguments
+ * Return: always 0
+ */
+
+int main(int argc, char  *argv[])
+{
+	int i;
+	int result = 0;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			if (isString(argv[i]))
+			{
+				result += atoi(argv[i]);
+			}
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		printf("%d\n", result);
+		return (0);
+	}
+	else
+	{
+		printf("%d\n", 0);
+		return (1);
+	}
+}
