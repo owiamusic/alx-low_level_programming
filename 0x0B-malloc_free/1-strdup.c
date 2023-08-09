27b348263148e1bfe41f7ea4a0856189ccf9109d
owiamusic <owiamusic@gmail.com>
2023-08-08 19:01:50 -0700

    function pointer

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+  * _strdup - Function that returns a pointer to newly allocated space
+  * @str: String to check
+  * Return: Null or Pointer
+  */
+
+char *_strdup(char *str)
+{
+	char *newString;
+	int i;
+	int j = 0;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	for (; str[j] != '\0'; j++)
+	{
+		newString = malloc(sizeof(char) * j + 1);
+	}
+	if (newString == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < j; i++)
+	{
+		newString[i] = str[i];
+	}
+		return (newString);
+
+}
+
