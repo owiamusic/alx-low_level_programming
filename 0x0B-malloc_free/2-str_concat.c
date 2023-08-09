b022fdd2bc4c0e8f53fa5ae556757631303e8574
owiamusic <owiamusic@gmail.com>
2023-08-09 07:18:42 -0700

    concatenate strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -20,21 +20,17 @@ char *str_concat(char *s1, char *s2)
 
 	if (s2 == NULL)
 		s2 = "";
-	
+
 	while (s1[len1] != '\0')
-	{
 		len1++;
-	}
 
 	while (s2[len2] != '\0')
-	{
 		len2++;
-	}
+
 	strNew = malloc(sizeof(char) * (len1 + len2 + 1));
 	if (strNew == NULL)
-	{
 		return (NULL);
-	}
+
 	temp = strNew;
 
 	while (*s1 != '\0')
