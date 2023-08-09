1254c604776dd5c0e4af86753979fd3eb8bf861b
owiamusic <owiamusic@gmail.com>
2023-08-09 13:24:55 -0700

    pointer to 2 dimensional arrays

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -24,7 +24,7 @@ int **alloc_grid(int width, int height)
 
 	for (i = 0; i < height; i++)
 	{
-		newInt[i] = (int *)malloc(sizeof(int *) * width);
+		newInt[i] = (int *)malloc(sizeof(int) * width);
 		if (newInt[i] == NULL)
 		{
 			for (j = 0; j < i; j++)
