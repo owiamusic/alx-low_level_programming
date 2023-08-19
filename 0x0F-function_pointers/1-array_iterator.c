c600a05f03aec5f1e8ca39451a4f5bcdef33cd4e
owiamusic <owiamusic@gmail.com>
2023-08-18 18:10:10 -0700

    function that executes a function given as a parameter

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -9,12 +9,14 @@
  * @action: A pointer to the function used to print
  */
 
-void array_iterator(int *array,	size_t size, void (*action)(int))
+void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
 
-	if (array != NULL && size != 0 && action != NULL)
-		for (i = 0; i < size; i++)
-			action(array[i]);
+	if (array == NULL || action == NULL)
+		exit(98);
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
 }
-
