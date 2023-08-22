f81a028c46f93dbba6931937856fd3a855e42133
owiamusic <owiamusic@gmail.com>
2023-08-22 12:42:55 -0700

    search integers

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+  * int_index - searches for an integer
+  * @array: array
+  * @size: number of elemers in the array
+  * @cmp: pointer to the function to be used
+  * Return: Boolean
+  */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <=0 || array == NULL || cmp == NULL)
+	{
+		return (-1);
+	}
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+		{
+			return (i);
+		}
+	}
+	return (-1);
+}
