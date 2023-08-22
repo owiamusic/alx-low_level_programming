bc464e89d85492ad68369198683e3320d6080754
owiamusic <owiamusic@gmail.com>
2023-08-22 12:16:42 -0700

    print name

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -2,6 +2,5 @@
 #define _FUNCTION_POINTERS_H
 
 void print_name(char *name, void (*f)(char *));
-void array_iterator(int *array, size_t size, void (*action)(int));
 
 #endif /** End Function Pointers */
