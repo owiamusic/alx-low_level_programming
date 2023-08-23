b4a060c38cc6a0843d32af1792e9721f37371341
owiamusic <owiamusic@gmail.com>
2023-08-23 05:43:02 -0700

    variadic functions

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -12,14 +12,12 @@ int sum_them_all(const unsigned int n, ...)
 	unsigned int i;
 	int sum;
 
-	if (n == 0)
-	{
-		return (0);
-	}
 	va_start(args, n);
 	for (i = 0; i < n; i++)
 	{
 		sum += va_arg(args, int);
+		if (n == 0)
+			return (0);
 	}
 	va_end(args);
 	return (sum);
