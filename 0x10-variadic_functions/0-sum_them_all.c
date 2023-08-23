5aae768b76ac6453b64b24db5ad6d07a00ea5413
owiamusic <owiamusic@gmail.com>
2023-08-23 05:55:35 -0700

    sum arguments

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -8,17 +8,20 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list args;
 	unsigned int i;
-	int sum;
+	int sum = 0;
 
-	va_start(args, n);
+	va_list sumArg;
+
+	if (n == 0)
+	{
+		return (0);
+	}
+	va_start(sumArg, n);
 	for (i = 0; i < n; i++)
 	{
-		sum += va_arg(args, int);
-		if (n == 0)
-			return (0);
+		sum += va_arg(sumArg, unsigned int);
 	}
-	va_end(args);
+	va_end(sumArg);
 	return (sum);
 }
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,6 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
+#include <stdlib.h>
 #include <stdio.h>
 #include <stdarg.h>
 
