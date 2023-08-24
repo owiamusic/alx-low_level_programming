2650cf997380b545f65c243fa5dbddd76a21ba6e
owiamusic <owiamusic@gmail.com>
2023-08-24 13:39:40 -0700

    print strings

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,33 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_strings - Print strings followed by new line
+ * @separator: string to be printed between
+ * @n: strings passed into function
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list printStr;
+	unsigned int i;
+	char *strArg;
+
+	if (separator == NULL)
+		separator = "";
+
+	va_start(printStr, n);
+	for (i = 0; i < n; i++)
+	{
+		if (strArg == NULL)
+			printf("(nil)");
+		else
+			strArg += printf("%s", va_arg(printStr, char *));
+		if (i < n - 1)
+			printf("%s", separator);
+	}
+	va_end(printStr);
+	printf("\n");
+}
+
