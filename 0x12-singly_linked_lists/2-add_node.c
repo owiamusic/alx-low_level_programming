7693b8668204e0dde40504fea1cc1ac66338b624
owiamusic <owiamusic@gmail.com>
2023-08-24 18:02:21 -0700

    Adds new node

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * add_node - Adds a new node
+ * @head: head of address
+ * @str: string to input
+ * Return: Address od new node
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+	int i = 0;
+
+	while (str[i])
+		i++;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	new->str = strdup(str);
+	new->len = i;
+	new->next = *head;
+
+	*head = new;
+
+	return (new);
+}
